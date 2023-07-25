7684ec62f5dd366c1b0307880d487229877506ec
Kfir <kfir.levy@themarker.com>
2023-07-25 12:44:47 +0300

    add constructor and setters

--- src/main/java/com/sso/SSOCookie.java
+++ src/main/java/com/sso/SSOCookie.java
@@ -11,67 +11,92 @@ public class SSOCookie {
     private String antiAbuseToken;
     private String ticketId;
 
+    public SSOCookie() {
+    }
+
+    public SSOCookie(String userId, SSOUserType userType, String userMail,
+                     SSOEmailValidity emailValidity, String firstName, String lastName,
+                     String antiAbuseToken, String ticketId) {
+
+        this.userId = userId;
+        this.userType = userType;
+        this.userMail = userMail;
+        this.emailValidity = emailValidity;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.antiAbuseToken = antiAbuseToken;
+        this.ticketId = ticketId;
+    }
+
     public String getUserId() {
         return userId;
     }
 
-    void setUserId(String userId) {
+    public SSOCookie setUserId(String userId) {
         this.userId = userId;
+        return this;
     }
 
     public SSOUserType getUserType() {
         return userType;
     }
 
-    void setUserType(SSOUserType userType) {
+    public SSOCookie setUserType(SSOUserType userType) {
         this.userType = userType;
+        return this;
     }
 
     public String getUserMail() {
         return userMail;
     }
 
-    void setUserMail(String userMail) {
+    public SSOCookie setUserMail(String userMail) {
         this.userMail = userMail;
+        return this;
     }
 
     public SSOEmailValidity getEmailValidity() {
         return emailValidity;
     }
 
-    void setEmailValidity(SSOEmailValidity emailValidity) {
+    public SSOCookie setEmailValidity(SSOEmailValidity emailValidity) {
         this.emailValidity = emailValidity;
+        return this;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    void setFirstName(String firstName) {
+    public SSOCookie setFirstName(String firstName) {
         this.firstName = firstName;
+        return this;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    void setLastName(String lastName) {
+    public SSOCookie setLastName(String lastName) {
         this.lastName = lastName;
+        return this;
     }
 
     public String getAntiAbuseToken() {
         return antiAbuseToken;
     }
 
-    void setAntiAbuseToken(String antiAbuseToken) {
+    public SSOCookie setAntiAbuseToken(String antiAbuseToken) {
         this.antiAbuseToken = antiAbuseToken;
+        return this;
     }
 
     public String getTicketId() {
         return ticketId;
     }
 
-    void setTicketId(String ticketId) {
+    public SSOCookie setTicketId(String ticketId) {
         this.ticketId = ticketId;
+        return this;
     }
 }
